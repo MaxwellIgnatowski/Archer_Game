656d1bb06894e880c856d91aab74d5746e0c23e7
Maxwell Ignatowski <Max53132@gmail.com>
2018-07-18 19:10:26 -0500

    Added character class

---
+++ src/com/company/Character.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Character {
+    private int position;
+
+    public Character() {
+        position = 0;
+    }
+
+    public void moveLeft() {
+        position++;
+        //more code here
+    }
+
+    public void moveRight() {
+        position--;
+        //more code here
+    }
+
+}
