eb250765681208e8f3a551b77b653a6b2542ead4
Maxwell Ignatowski <Max53132@gmail.com>
2018-07-20 19:41:15 -0500

    Restructured connection for Overworld

--- src/com/company/OverworldBase.java
+++ src/com/company/OverworldBase.java
@@ -5,6 +5,10 @@ import java.util.Map;
 public class OverworldBase {
 
     public Tile[][] tileSet;
-    public Map<Integer, OverworldConnection> connections;
+    public Map<Integer, OverworldBase> connections;
+
+
+    // check at every position the user is at, if it matches a value in the hashmap, and if it does
+    // then move to the matching OverworldBase
 
 }
