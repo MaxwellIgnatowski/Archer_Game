5cb07c3cef82db04a496b25e12ce4b8a2995d146
JTPenkalski <jbonepen@gmail.com>
2018-07-21 09:29:51 -0500

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,11 @@ public class Main {
 
     static BattleView userInterface;
 
-<<<<<<< HEAD
-    public static void main(String[] args) 
-    {
-	    Character juice = new Character();
-	    TestTitle test = new TestTitle();
-=======
     public static void main(String[] args) {
 	    System.out.println("This is the start!");
 	    System.out.println("Also... hello world!");
-
+	    
+	    TestTitle test = new TestTitle();
 	    BattleCharacter juice = new BattleCharacter();
-
-
->>>>>>> c894247d7a8722c103925a35588d2e85fe0532fc
     }
 }
