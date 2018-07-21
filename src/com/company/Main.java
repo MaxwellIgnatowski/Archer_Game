4c4eda6cf85a5e20b4dbbf7c33390d5cf25acdb0
Maxwell Ignatowski <Max53132@gmail.com>
2018-07-21 11:04:52 -0500

    Fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,8 @@ package com.company;
 
 public class Main {
 
-    static BattleView userInterface;
-
     public static void main(String[] args) {
     	BattleCharacter juice = new BattleCharacter();
-    	//GameOverScreen gos = new GameOverScreen("You have fallen bravely in battle."); - Try if you want...
+    	GameOverScreen gos = new GameOverScreen("You have fallen bravely in battle.");
     }
 }
